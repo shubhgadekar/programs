c1b0f4ef84e7e0d06280f1b306c2d307b00d3328
shubhgadekar <136062336+shubhgadekar@users.noreply.github.com>
2024-05-09 11:17:18 +0530

    Update Pandas_solution.py
    Exception handling added

--- Pandas_solution.py
+++ Pandas_solution.py
@@ -1,22 +1,37 @@
 import sqlite3
 import pandas as pd
-conn = sqlite3.connect('Data_Engineer_ETL_Assignment.db')
 
-cursor = conn.cursor()
+try:
+    conn = sqlite3.connect('Data_Engineer_ETL_Assignment.db')
 
-query = '''
-    SELECT c.customer_id as Customer, c.age as Age, i.item_name, SUM(o.quantity) AS Total_quantity
-    FROM Customers c
-    JOIN Sales s ON c.customer_id = s.customer_id
-    JOIN Orders o ON s.sales_id = o.sales_id
-    JOIN Items i ON o.item_id = i.item_id
-    WHERE c.age >= 18 AND c.age <= 35
-    GROUP BY c.customer_id, i.item_name
-    HAVING total_quantity > 0
-'''
+    cursor = conn.cursor()
 
-df = pd.read_sql_query(query, conn)
+    query = '''
+        SELECT c.customer_id as Customer, c.age as Age, i.item_name, SUM(o.quantity) AS Total_quantity
+        FROM Customers c
+        JOIN Sales s ON c.customer_id = s.customer_id
+        JOIN Orders o ON s.sales_id = o.sales_id
+        JOIN Items i ON o.item_id = i.item_id
+        WHERE c.age >= 18 AND c.age <= 35
+        GROUP BY c.customer_id, i.item_name
+        HAVING total_quantity > 0
+    '''
 
-df.to_csv('customer_item_quantities.csv', sep=';', index=False)
+    df = pd.read_sql_query(query, conn)
 
-conn.close()
+    df.to_csv('customer_item_quantities.csv', sep=';', index=False)
+
+except sqlite3.Error as e:
+    print("SQLite error occurred:", e)
+
+except pd.errors.ParserError as e:
+    print("Pandas error occurred:", e)
+
+except Exception as e:
+    print("An error occurred:", e)
+
+finally:
+    try:
+        conn.close()
+    except NameError:
+        pass
